fa30b5c46a151d53e17490ca179d833db986b6b5
Charles Ci-Jyun Wu <dminus@andestech.com>
2023-09-14 21:24:21 +0800

    riscv: andes: add andes vendor sbi fid to include/linux/soc/andes/sbi.h
    Andes has defined SBI function IDs within the vendor ID architecture.
    
    Signed-off-by: Charles Ci-Jyun Wu <dminus@andestech.com>

--- arch/riscv/errata/andes/errata.c
+++ arch/riscv/errata/andes/errata.c
@@ -15,24 +15,21 @@
 #include <asm/errata_list.h>
 #include <asm/patch.h>
 #include <asm/processor.h>
-#include <asm/sbi.h>
 #include <asm/vendorid_list.h>
+#include <linux/soc/andes/sbi.h>
 
 #define ANDES_AX45MP_MARCHID		0x8000000000008a45UL
 #define ANDES_AX45MP_MIMPID		0x500UL
-#define ANDES_SBI_EXT_ANDES		0x0900031E
-
-#define ANDES_SBI_EXT_IOCP_SW_WORKAROUND	1
 
 static long ax45mp_iocp_sw_workaround(void)
 {
 	struct sbiret ret;
 
 	/*
-	 * ANDES_SBI_EXT_IOCP_SW_WORKAROUND SBI EXT checks if the IOCP is missing and
+	 * SBI_EXT_ANDES_IOCP_SW_WORKAROUND SBI EXT checks if the IOCP is missing and
 	 * cache is controllable only then CMO will be applied to the platform.
 	 */
-	ret = sbi_ecall(ANDES_SBI_EXT_ANDES, ANDES_SBI_EXT_IOCP_SW_WORKAROUND,
+	ret = sbi_ecall(ANDES_SBI_EXT_ANDES, SBI_EXT_ANDES_IOCP_SW_WORKAROUND,
 			0, 0, 0, 0, 0, 0);
 
 	return ret.error ? 0 : ret.value;
