206b2184419696eb9cded833003c62183f28ae1e
Charles Ci-Jyun Wu <dminus@andestech.com>
2023-09-14 21:24:21 +0800

    riscv: andes: add andes vendor sbi fid to include/linux/soc/andes/sbi.h
    Andes has defined SBI function IDs within the vendor ID architecture.
    
    Signed-off-by: Charles Ci-Jyun Wu <dminus@andestech.com>

---
+++ include/linux/soc/andes/sbi.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2023 Andes Technology Corporation.
+ */
+#ifndef __LINUX_SOC_ANDES_SBI_H
+#define __LINUX_SOC_ANDES_SBI_H
+
+#include <asm/sbi.h>
+#include <asm/vendorid_list.h>
+
+#define ANDES_SBI_EXT_ANDES	0x0900031E
+
+/*
+ * Set Andes's internal SBI Call FID start at 64 to avoid the impact of
+ * upstream add or remove in sbi_ext_andes_fid{}. If the FID numbers
+ * change, our released OpenSBI will lose backward compatible.
+ */
+#define ANDES_SBI_INTERNAL_FID_START 64
+
+enum sbi_ext_andes_fid {
+	SBI_EXT_ANDES_FID0 = 0, /* Reserved for future use */
+	SBI_EXT_ANDES_IOCP_SW_WORKAROUND,
+
+	/* Trace */
+	SBI_EXT_ANDES_TRIGGER_SET = ANDES_SBI_INTERNAL_FID_START,
+
+	/* CPU freq */
+	SBI_EXT_ANDES_POWERBRAKE_READ,
+	SBI_EXT_ANDES_POWERBRAKE_WRITE,
+
+	/* Programmable physical memory attributes (PPMA) */
+	SBI_EXT_ANDES_PMA_SET,
+	SBI_EXT_ANDES_PMA_FREE,
+	SBI_EXT_ANDES_PMA_PROBE,
+
+	SBI_EXT_ANDES_DCACHE_EN,
+
+	/* CPU idle (ATCSMU) */
+	SBI_EXT_ANDES_SUSPEND_MODE_SET,
+	SBI_EXT_ANDES_SUSPEND_MODE_ENTER,
+};
+
+#endif /* !__LINUX_SOC_ANDES_SBI_H */
