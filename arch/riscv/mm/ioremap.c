ef1b9ad6d3bb13edf4cff96d4659056e76ba54d1
charles <dminus@andestech.com>
2024-04-26 13:28:15 +0800

    soc: andes: ppma: support Andes Programmable Physical Memory Attribute (PPMA)
    If the device want a non-cacheable memory region, it calls
    ioremap_wc() to get a VA which maps to non-cacheable memory region.
    The CONFIG_ANDES_PPMA must be set.
    
    Reformed from the following patches on RISCV-Linux-5.4:
    - riscv: Support PMA(Physical Memory Attribute) on linux5.4.
    - FTLCD100: Fix ioremap_nocache() causing LCD kernel panic issue.
    - Complete the programmable PMA implementation with IPI
    - Fix compilation error for (884f011d)
    - andes: rename sbi calls of andes vendor extension
    - riscv: fix compilation warnings
    - RISC-V: skip PMA setting for non-memory region
    
    Reformed from the following patches on RISCV-Linux-6.1:
    - soc: andes: ppma: support Andes PPMA(Programmable Physical Memory
      Attribute) in Linux 6.1
    - soc: andes: ppma: Support IPI in andes_free_ppma()
    - soc: andes: ppma: fix complier error when disable ppma
    - soc: andes: ppma:Replacing smp_processor_id() with get/put_cpu()
      for preemption control
    
    Signed-off-by: charles <dminus@andestech.com>

--- arch/riscv/mm/ioremap.c
+++ arch/riscv/mm/ioremap.c
@@ -6,6 +6,8 @@
 #include <linux/export.h>
 #include <linux/io.h>
 #include <linux/pgtable.h>
+#include <linux/soc/andes/andes.h>
+#include <linux/soc/andes/ppma.h>
 
 void __iomem *ioremap_wc(phys_addr_t phys_addr, size_t size)
 {
@@ -16,12 +18,18 @@ void __iomem *ioremap_wc(phys_addr_t phys_addr, size_t size)
 
 	ret = ioremap_prot(phys_addr, size, pgprot_val(prot));
 
+	if (ret && !andes_pfn_msb)
+		andes_set_ppma(phys_addr, ret, size);
+
 	return ret;
 }
 EXPORT_SYMBOL(ioremap_wc);
 
 void iounmap(volatile void __iomem *addr)
 {
+	if (!andes_pfn_msb)
+		andes_free_ppma((void *)addr);
+
 	generic_iounmap(addr);
 }
 EXPORT_SYMBOL(iounmap);
