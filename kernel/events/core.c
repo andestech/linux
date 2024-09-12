f62b1c9c443a5d4e06fe611b220dfa3286e700e7
Ben Zong-You Xie <ben717@andestech.com>
2024-09-12 13:59:40 +0800

    kernel: events: fix RCU stall and prevent triggering warning for Perf (#183)
    Port two workaround patches for Perf from ast-v5_3_0-branch.
    
    - Commit ad6833cc66 (perf/core: Implement workaround to fix RCU stall issue
    when using perf record) is for RCU stall problem
    - Commit 84c459a831 (perf/core: Implement workaround to add pmu stop before
    unthrottling to prevent WARNING) is to prevent triggering warning
    
    Co-authored-by: Locus Wei-Han Chen <locus84@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/183
    Reviewed-by: Charles Ci-Jyun Wu <dminus@andestech.com>
    Reviewed-by: randolph <randolph@andestech.com>
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
    Co-authored-by: Ben Zong-You Xie <ben717@andestech.com>
    Co-committed-by: Ben Zong-You Xie <ben717@andestech.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -4240,6 +4240,12 @@ static void perf_adjust_freq_unthr_events(struct list_head *event_list)
 
 		if (hwc->interrupts == MAX_INTERRUPTS) {
 			hwc->interrupts = 0;
+
+			/*
+			 * NOTE: this is a workaround to prevent triggering
+			 * WARN_ON_ONCE().
+			 */
+			event->pmu->stop(event, 0);
 			perf_log_throttle(event, 1);
 			if (!event->attr.freq || !event->attr.sample_freq)
 				event->pmu->start(event, 0);
@@ -9732,8 +9738,15 @@ __perf_event_account_interrupt(struct perf_event *event, int throttle)
 
 		hwc->freq_time_stamp = now;
 
-		if (delta > 0 && delta < 2*TICK_NSEC)
-			perf_adjust_period(event, delta, hwc->last_period, true);
+		/* NOTE: this is a workaround for RCU stall problem */
+		event->pmu->stop(event, PERF_EF_UPDATE);
+		u64 now_count = local64_read(&event->count);
+		s64 delta_count = now_count - hwc->freq_count_stamp;
+		hwc->freq_count_stamp = now_count;
+
+		if ((delta > 0) && (delta < 2 * TICK_NSEC) && (delta_count > 0))
+			perf_adjust_period(event, delta, delta_count, true);
+		event->pmu->start(event, delta_count > 0 ? PERF_EF_RELOAD : 0);
 	}
 
 	return ret;
