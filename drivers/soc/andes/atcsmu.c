b49b2d15283038e53933467f8d2b98cfbb316a78
Leo Yu-Chi Liang <ycliang@andestech.com>
2024-09-26 13:34:50 +0800

    smu: andes: atcsmu: Add Andes ATCSMU driver support (#195)
    Add basic atcsmu driver support for AE350 platform.
    
    Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/195
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
    Reviewed-by: Charles Ci-Jyun Wu <dminus@andestech.com>
    Co-authored-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Co-committed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

---
+++ drivers/soc/andes/atcsmu.c
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2024 Andes Technology Corporation.
+ */
+#define pr_fmt(fmt) "atcsmu: " fmt
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/soc/andes/smu.h>
+
+static struct atcsmu atcsmu = { 0 };
+
+void __iomem *atcsmu_get_address(void)
+{
+	struct atcsmu *smu = &atcsmu;
+	return smu->base;
+}
+
+static int atcsmu_probe(struct platform_device *pdev)
+{
+	struct atcsmu *smu = &atcsmu;
+	unsigned int cpu;
+
+	smu->base = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(smu->base))
+		return PTR_ERR(smu->base);
+
+	for_each_possible_cpu(cpu)
+		writel(0x0, (void *)(smu->base + PCSm_WE_OFF(cpu)));
+
+	pr_info("ATCSMU driver probed\n");
+
+	return 0;
+}
+
+static int __exit atcsmu_remove(struct platform_device *pdev)
+{
+	pr_info("ATCSMU driver removed\n");
+	return 0;
+}
+
+static const struct of_device_id atcsmu_of_id_table[] = {
+	{ .compatible = "andestech,atcsmu" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, atcsmu_of_id_table);
+
+static struct platform_driver atcsmu_driver = {
+	.probe = atcsmu_probe,
+	.remove = __exit_p(atcsmu_remove),
+	.driver = {
+		.name = "atcsmu",
+		.of_match_table = of_match_ptr(atcsmu_of_id_table),
+	},
+};
+
+static int __init atcsmu_init(void)
+{
+	int ret = platform_driver_register(&atcsmu_driver);
+
+	return ret;
+}
+subsys_initcall(atcsmu_init);
