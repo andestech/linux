4dc30e249849bde9d93dbd9933b3720f4b46ea42
charles <dminus@andestech.com>
2024-10-18 18:09:45 +0800

    soc: andes: hvm: support Andes HVM DMA driver (#212)
    Co-authored-by: CL Wang <cl634@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/212
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
    Co-authored-by: charles <dminus@andestech.com>
    Co-committed-by: charles <dminus@andestech.com>

--- drivers/dma/atcdmac300.c
+++ drivers/dma/atcdmac300.c
@@ -488,11 +488,19 @@ static inline unsigned int xfer_width(struct v5_dma *v5dma,
 {
 	unsigned int width;
 
-	if (!((src | dst  | len) & 15))
+	/*
+	 * To prevent HVM DMA transfer from getting stuck,
+	 * limit the transfer width to word transfer.
+	 */
+# if 0
+	if (!((src | dst | len) & 15))
 		width = 4;
 	else if (!((src | dst | len) & 7))
 		width = 3;
 	else if (!((src | dst | len) & 3))
+#else
+	if (!((src | dst | len) & 3))
+#endif
 		width = 2;
 	else if (!((src | dst | len) & 1))
 		width = 1;
