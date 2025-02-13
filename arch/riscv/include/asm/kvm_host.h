e80de218ffcace104e9df86ab6010d2416a24979
Leo Yu-Chi Liang <ycliang@andestech.com>
2025-02-13 19:51:40 +0800

    [Backport] RISC-V KVM Smstateen (#251)
    Backported RISC-V KVM Smstateen patch series to support bringing up guest OS that support AIA extension.
    
    Co-authored-by: Mayuresh Chitale <mchitale@ventanamicro.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/251
    Reviewed-by: Charles Ci-Jyun Wu <dminus@andestech.com>
    Reviewed-by: Mina Hui-Min Chou <minachou@andestech.com>
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>

--- arch/riscv/include/asm/kvm_host.h
+++ arch/riscv/include/asm/kvm_host.h
@@ -165,6 +165,15 @@ struct kvm_vcpu_csr {
 	unsigned long senvcfg;
 };
 
+struct kvm_vcpu_config {
+	u64 henvcfg;
+	u64 hstateen0;
+};
+
+struct kvm_vcpu_smstateen_csr {
+	unsigned long sstateen0;
+};
+
 struct kvm_vcpu_arch {
 	/* VCPU ran at least once */
 	bool ran_atleast_once;
@@ -185,6 +194,7 @@ struct kvm_vcpu_arch {
 	unsigned long host_stvec;
 	unsigned long host_scounteren;
 	unsigned long host_senvcfg;
+	unsigned long host_sstateen0;
 
 	/* CPU context of Host */
 	struct kvm_cpu_context host_context;
@@ -194,6 +204,9 @@ struct kvm_vcpu_arch {
 
 	/* CPU CSR context of Guest VCPU */
 	struct kvm_vcpu_csr guest_csr;
+
+	/* CPU Smstateen CSR context of Guest VCPU */
+	struct kvm_vcpu_smstateen_csr smstateen_csr;
 
 	/* CPU context upon Guest VCPU reset */
 	struct kvm_cpu_context guest_reset_context;
@@ -246,6 +259,9 @@ struct kvm_vcpu_arch {
 
 	/* Performance monitoring context */
 	struct kvm_pmu pmu_context;
+
+	/* 'static' configurations which are set only once */
+	struct kvm_vcpu_config cfg;
 };
 
 static inline void kvm_arch_sync_events(struct kvm *kvm) {}
