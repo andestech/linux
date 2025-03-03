06918008066e441d4034b63e5169539062c1624a
Locus Wei-Han Chen <locus84@andestech.com>
2025-03-03 16:37:47 +0800

    andes: spi: Fixed the bug that mutex is used without init (#259)
    Fixed the bug that mutex is used without init.
    
    Fixes: Bugzilla 33642 ("[ASTv540] Fixed the bug that mutex is used without init")
    http://e-andes.andestech.com/bugzilla5/show_bug.cgi?id=33642
    
    Signed-off-by: Locus Wei-Han Chen <locus84@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/259
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
    Reviewed-by: Alex Chun-Ju Lin <alex749@andestech.com>
    Co-authored-by: Locus Wei-Han Chen <locus84@andestech.com>
    Co-committed-by: Locus Wei-Han Chen <locus84@andestech.com>

--- drivers/spi/spi-atcspi200.c
+++ drivers/spi/spi-atcspi200.c
@@ -643,6 +643,7 @@ static int atcspi200_spi_probe(struct platform_device *pdev)
 		num_cs = NSPI_MAX_CS_NUM;
 	}
 
+	mutex_init(&spi->mutex_lock);
 	/* Define our master */
 	master->bus_num = pdev->id;
 	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_RX_QUAD | SPI_TX_QUAD;
