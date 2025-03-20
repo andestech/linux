62f0764ee0753220b21eb7da6c5aac0778c61e61
Cynthia Lan-Ya Huang <cynthia@andestech.com>
2025-03-20 15:45:11 +0800

    riscv: andes: errata: 25 series CPU use flush_icache_all (#265)
    Fix: bugzilla #33944 (http://e-andes.andestech.com/bugzilla5/show_bug.cgi?id=33944)
    
    The LTP test suite will fail without this patch.
    
    Signed-off-by: Charles Wu <dminus@andestech.com>
    Co-authored-by: Charles Wu <dminus@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/265
    Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Co-authored-by: Cynthia Lan-Ya Huang <cynthia@andestech.com>
    Co-committed-by: Cynthia Lan-Ya Huang <cynthia@andestech.com>

--- arch/riscv/mm/cacheflush.c
+++ arch/riscv/mm/cacheflush.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/of.h>
+#include <linux/soc/andes/andes.h>
 #include <asm/cacheflush.h>
 
 #ifdef CONFIG_SMP
@@ -85,7 +86,10 @@ void flush_icache_pte(struct mm_struct *mm, pte_t pte)
 	struct folio *folio = page_folio(pte_page(pte));
 
 	if (!test_bit(PG_dcache_clean, &folio->flags)) {
-		flush_icache_mm(mm, false);
+		if (static_branch_unlikely(&andes_legacy_mmu_key))
+			flush_icache_all();
+		else
+			flush_icache_mm(mm, false);
 		set_bit(PG_dcache_clean, &folio->flags);
 	}
 }
