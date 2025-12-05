d5e861f74ba6017fe829c176d2f15483644a3e51
Hui Min Mina Chou <minachou@andestech.com>
2025-12-05 16:54:08 +0800

    RISC-V: KVM: Flush VS-stage TLB after VCPU migration for Andes cores (#326)
    Most implementations cache the combined result of two-stage translation,
    but some, like Andes cores, use split TLBs that store VS-stage and
    G-stage entries separately.
    
    On such systems, when a VCPU migrates to another CPU, an additional
    HFENCE.VVMA is required to avoid using stale VS-stage entries, which
    could otherwise cause guest faults.
    
    Introduce a static key to identify CPUs with split two-stage TLBs.
    When enabled, KVM issues an extra HFENCE.VVMA on VCPU migration to
    prevent stale VS-stage mappings.
    
    Fixes: Bug 36481 - [ASTv542] GuestOS crashes with segmentation fault on AX66 2C bitmap
    (http://e-andes.andestech.com/bugzilla5/show_bug.cgi?id=36481)
    
    Signed-off-by: Hui Min Mina Chou <minachou@andestech.com>
    Signed-off-by: Ben Zong-You Xie <ben717@andestech.com>
    Reviewed-by: Radim Krčmář <rkrcmar@ventanamicro.com>
    Reviewed-by: Nutty Liu <nutty.liu@hotmail.com>
    Link: https://lore.kernel.org/r/20251117084555.157642-1-minachou@andestech.com
    Signed-off-by: Anup Patel <anup@brainfault.org>
    
    Reviewed-on: https://gitea.andestech.com/RD-SW/linux/pulls/326
    Co-authored-by: Hui Min Mina Chou <minachou@andestech.com>
    Co-committed-by: Hui Min Mina Chou <minachou@andestech.com>

--- arch/riscv/include/asm/kvm_host.h
+++ arch/riscv/include/asm/kvm_host.h
@@ -376,4 +376,7 @@ void __kvm_riscv_vcpu_power_on(struct kvm_vcpu *vcpu);
 void kvm_riscv_vcpu_power_on(struct kvm_vcpu *vcpu);
 bool kvm_riscv_vcpu_stopped(struct kvm_vcpu *vcpu);
 
+/* Flags representing implementation specific details */
+DECLARE_STATIC_KEY_FALSE(kvm_riscv_vsstage_tlb_no_gpa);
+
 #endif /* __RISCV_KVM_HOST_H__ */
